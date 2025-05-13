0959fb501a14316fb2aa46a1688f25c3658867a9
zsohajdu1 <zsohajdu1@gmail.com>
2025-05-13 18:32:18 +0200

    added docker compose

--- fitness/src/main/java/aut/bme/hu/fitness/config/SecurityConfig.java
+++ fitness/src/main/java/aut/bme/hu/fitness/config/SecurityConfig.java
@@ -2,17 +2,9 @@ package aut.bme.hu.fitness.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
